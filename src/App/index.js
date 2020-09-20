7353adb7057909fa47459098a303f20d1c5bdb3a
Rıdvan Karadağ <dev@rootvan.com>
2020-09-20 15:47:55 +0300

    removed unnecessary code blocks and tests

--- src/App/index.js
+++ src/App/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
-
 import appStyles from './index.module.css'
 
 function App() {
   return (
     <div className={appStyles.app}>
-      <header className={appStyles.header}>
-        <img src={logo} className={appStyles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={appStyles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
