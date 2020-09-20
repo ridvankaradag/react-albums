fa07dffa7abcac53fdb4142ff4ca4052df893907
Rıdvan Karadağ <dev@rootvan.com>
2020-09-20 21:00:20 +0300

    added basic ui with firebase

--- src/screens/Gallery/index.js
+++ src/screens/Gallery/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as firebase from 'firebase';
+
+import styles from './index.module.css'
 
 const Gallery = () => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        handleFirebase()
+    }, []);
+
+    const handleFirebase = () => {
+        firebase.database().ref('urls').once('value').then((snapshot) => {
+            const galleryObj = snapshot.val();
+            const gallery = Object.keys(galleryObj).filter(key => !images.includes(key)).map(key => ({
+                ...galleryObj[key],
+                id: key,
+              }));
+            console.log(gallery)
+            setImages(gallery)   
+        })
+        
+    }
     return (
-        <h1>Gallery</h1>
+        <div className={styles.gallery}>
+            {
+                images.map(image => (
+                    <div className={styles.item} key={image.id}>
+                        <img src={image.url} alt={`Gallery Item ${image.id}`} className={styles.image}/>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
