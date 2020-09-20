38e610faf16a020eec3ce57e8f9c1a9bccde9ada
Rıdvan Karadağ <dev@rootvan.com>
2020-09-20 16:47:02 +0300

    added drag&drop events

--- src/components/PhotoUpload/index.js
+++ src/components/PhotoUpload/index.js
@@ -4,6 +4,33 @@ import styles from './index.module.css';
 
 const PhotoUpload = () => {
 
+    const handleDragEnter = e => {
+        e.preventDefault();
+        e.stopPropagation();
+      };
+      const handleDragLeave = e => {
+        e.preventDefault();
+        e.stopPropagation();
+      };
+      const handleDragOver = e => {
+        e.preventDefault();
+        e.stopPropagation();
+      };
+      const handleDrop = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        let files = [...e.dataTransfer.files];
+
+        console.log('files', files);
+  
+        if (files && files.length > 0) {
+            const existingFiles = []
+            files = files.filter(f => !existingFiles.includes(f.name))
+            
+            e.dataTransfer.clearData();
+        }
+      }
+
       const handleInputChange = e => {
         e.preventDefault();
         e.stopPropagation();
@@ -13,6 +40,10 @@ const PhotoUpload = () => {
     return (
         <form className={styles.form} 
             encType="multipart/form-data"
+            onDrop={e => handleDrop(e)}
+            onDragOver={e => handleDragOver(e)}
+            onDragEnter={e => handleDragEnter(e)}
+            onDragLeave={e => handleDragLeave(e)}
         >
             <div className={styles.inputContainer}>
                 <input onChange={e => handleInputChange(e)} className={styles.input} type="file" name="files[]" id="file" multiple />
